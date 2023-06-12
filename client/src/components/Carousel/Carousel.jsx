23df51c4c25d3a7d3dff88c59b476e872a890d79
Majomon <mauri.monzon91@gmail.com>
2023-06-12 16:25:03 -0300

    Carousel

--- client/src/components/Carousel/Carousel.jsx
+++ client/src/components/Carousel/Carousel.jsx
@@ -1,12 +1,27 @@
-import React from "react";
-import style from "./Carousel.module.css";
+import React, { useState, useEffect } from 'react';
 
-function Carousel() {
+const Carousel = ({ images }) => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      // Avanzar al siguiente índice de imagen
+      setCurrentImageIndex((prevIndex) =>
+        prevIndex === images.length - 1 ? 0 : prevIndex + 1
+      );
+    }, 2000);
+
+    return () => {
+      // Limpiar el intervalo cuando el componente se desmonta
+      clearInterval(interval);
+    };
+  }, [images]);
+
   return (
-    <div className={style.container}>
-      <h1>Soy un lindo carousel</h1>
+    <div className="carousel">
+      <img src={images[currentImageIndex]} alt="Carousel Image" />
     </div>
   );
-}
+};
 
 export default Carousel;
--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
+import Carousel from "../Carousel/Carousel";
+import img1 from "../../assets/cinego_blanco.png";
+import img2 from "../../assets/cinego_negro.png";
+import img3 from "../../assets/cineLogo.png";
 
 function Landing() {
+  const images = [
+    img1,
+    img2,
+    img3,
+    // Agrega más imágenes aquí
+  ];
   return (
     <div>
       SOY EL LANDING
+      <Carousel images={images} />
     </div>
-  )
+  );
 }
 
-export default Landing
+export default Landing;
