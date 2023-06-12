4b6160cf3366791e04345a1a148457ff270a35ef
Majomon <mauri.monzon91@gmail.com>
2023-06-12 16:12:26 -0300

    Initial commit

---
+++ client/src/components/Carousel/Carousel.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import style from "./Carousel.module.css";
+
+function Carousel() {
+  return (
+    <div className={style.container}>
+      <h1>Soy un lindo carousel</h1>
+    </div>
+  );
+}
+
+export default Carousel;
