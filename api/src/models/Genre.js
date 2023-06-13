ad5f97968d9d5c8ec85bdee9ac257c4d52ce0bd3
MatiasMedina02 <rusomedina02@gmail.com>
2023-06-13 10:17:13 -0400

    Base de datos conectada

---
+++ api/src/models/Genre.js
@@ -0,0 +1,16 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  sequelize.define("genre", {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    }, { timestamps: false }
+  )
+}
