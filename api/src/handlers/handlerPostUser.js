2447e4c6901c44442197e3a9df70e0c3b3e0e53a
Majomon <mauri.monzon91@gmail.com>
2023-06-14 20:58:53 -0300

    Arregle la ruta de postUser y Modifique MOvieCard

--- api/src/handlers/handlerPostUser.js
+++ api/src/handlers/handlerPostUser.js
@@ -3,7 +3,7 @@ const postUser = require("../controllers/postUserController")
 const handlerPostUser = async(req, res) => {
   try {
     const { firstName, lastName, email, password } = req.body 
-    const userData = await postUser(firstName, lastName, email, password)
+    const userData = await postUser({firstName, lastName, email, password})
     res.status(200).json(userData);
   } catch (error) {
       res.status(400).json({ error : error.message })
