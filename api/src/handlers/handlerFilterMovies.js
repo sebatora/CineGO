d2a4fcef00e75582ae2641371791272e7791bfd5
GeorgeDaz <you@example.com>
2023-06-15 12:57:57 -0600

    Jorge/FiltrosDeOrdenamientoListos

--- api/src/handlers/handlerFilterMovies.js
+++ api/src/handlers/handlerFilterMovies.js
@@ -1,40 +1,38 @@
-const getOrderAscending = require('../controllers/getOrderAscending');
-const getOrderDescending = require('../controllers/getOrderDescending');
-
+const getOrderAZ = require("../controllers/getOrderAZ");
+const getOrderAscending = require("../controllers/getOrderAscending");
+const getOrderDescending = require("../controllers/getOrderDescending");
+const getOrderZA = require("../controllers/getOrderZA");
 
 const handlerFilterMovies = async (req, res) => {
-    const { order, filterGenre, filterClasification } = req.body;
+  const { order, filterGenre, filterClasification } = req.body;
 
-    try{
-        let filtered = null;
-        
-        if (order){
-        
-         if (order === "ascending"){
-             filtered = await getOrderAscending();
-            // return res.status(200).json(ordered);
-            // order = 1 ascendente, descendente, reciente, antiguo
-        
-        } else if (order === "descending"){
-             filtered = await getOrderDescending();
-            // return res.status(200).json(ordered);
-        }
-        
-        // if(filterGenre){
-            //      console.log("hay generos")
-            // }
-            
-            // if(filterClasification){
-            //     console.log("estan clasificados")
-            // } ATP, +13, +16
-            
-        return res.status(200).json(filtered);
-        
-    }
+  try {
+    let filtered = null;
 
-    } catch (error) {
-      res.status(500).json({ error: 'Error filtrando.' });
+    if (order) {
+      if (order === "antiguo") {
+        filtered = await getOrderAscending();
+      } else if (order === "reciente") {
+        filtered = await getOrderDescending();
+      } else if (order === "ascendente") {
+        filtered = await getOrderAZ();
+      } else if (order === "descendente") {
+        filtered = await getOrderZA();
+      }
+
+      return res.status(200).json(filtered);
     }
+    // if(filterGenre){
+    //      console.log("hay generos")
+    // }
+
+    // if(filterClasification){
+    //     console.log("estan clasificados")
+    // } ATP, +13, +16
+    return res.status(200).json(filtered);
+  } catch (error) {
+    res.status(500).json({ error: "Error filtrando." });
+  }
 };
 
 module.exports = handlerFilterMovies;
