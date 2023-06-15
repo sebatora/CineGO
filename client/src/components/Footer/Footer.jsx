a2c32f6184ec10620e00b72f035085bf66372746
Sebatora <sebastiana.toranzo@gmail.com>
2023-06-15 17:29:48 -0300

    Cambios en footer y agregado de reducer al home

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 import '@fortawesome/fontawesome-free/css/all.css';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebookSquare, faTwitterSquare, faLinkedin, faInstagramSquare, faGithub, faReact, faNodeJs, faHtml5, faCss3Alt, faJs, faGitAlt, faTrello } from '@fortawesome/free-brands-svg-icons';
+import { faFacebookSquare, faTwitterSquare, faLinkedin, faInstagramSquare, faGithub, faReact, faNodeJs, faHtml5, faCss3Alt, faJs, faGitAlt, faTrello, faFigma } from '@fortawesome/free-brands-svg-icons';
 
 // Agrega los íconos al library
 library.add(faFacebookSquare, faTwitterSquare, faLinkedin, faInstagramSquare, faGithub, faReact, faNodeJs, faHtml5, faCss3Alt, faJs, faGitAlt, faTrello);
@@ -24,29 +24,30 @@ function Footer() {
           <div className={style.terms}>
             <p>Tecnologías utilizadas</p>
             <div className={style.iconContainer}>
-                <a href="https://es.react.dev"><FontAwesomeIcon icon={faReact} className={style.icon} /> </a>
-                <a href="https://nodejs.org/es"><FontAwesomeIcon icon={faNodeJs} className={style.icon} /> </a>
-                <a href="https://developer.mozilla.org/es/docs/Web/HTML"><FontAwesomeIcon icon={faHtml5} className={style.icon} /> </a>
-                <a href="https://developer.mozilla.org/es/docs/Web/CSS"><FontAwesomeIcon icon={faCss3Alt} className={style.icon} /> </a>
-                <a href="https://developer.mozilla.org/es/docs/Web/JavaScript"><FontAwesomeIcon icon={faJs} className={style.icon} /> </a>
-                <a href="https://git-scm.com"><FontAwesomeIcon icon={faGitAlt} className={style.icon}/> </a>
-                <a href="https://github.com"><FontAwesomeIcon icon={faGithub} className={style.icon}/> </a>
-                <a href="https://trello.com"><FontAwesomeIcon icon={faTrello} className={style.icon}/> </a>
+                <Link to="https://es.react.dev" target="_blank"><FontAwesomeIcon icon={faReact} className={style.icon} /> </Link>
+                <Link to="https://nodejs.org/es" target="_blank"><FontAwesomeIcon icon={faNodeJs} className={style.icon} /> </Link>
+                <Link to="https://developer.mozilla.org/es/docs/Web/HTML" target="_blank"><FontAwesomeIcon icon={faHtml5} className={style.icon} /> </Link>
+                <Link to="https://developer.mozilla.org/es/docs/Web/CSS" target="_blank"><FontAwesomeIcon icon={faCss3Alt} className={style.icon} /> </Link>
+                <Link to="https://developer.mozilla.org/es/docs/Web/JavaScript" target="_blank"><FontAwesomeIcon icon={faJs} className={style.icon} /> </Link>
+                <Link to="https://git-scm.com" target="_blank"><FontAwesomeIcon icon={faGitAlt} className={style.icon}/> </Link>
+                <Link to="https://github.com" target="_blank"><FontAwesomeIcon icon={faGithub} className={style.icon}/> </Link>
+                <Link to="https://trello.com" target="_blank"><FontAwesomeIcon icon={faTrello} className={style.icon}/> </Link>
+                <Link to="https://www.figma.com/" target="_blank"><FontAwesomeIcon icon={faFigma} className={style.icon}/> </Link>
             </div>
           </div>
         </div>
 
         <div className={style.box__footer}>
           <h2>Opciones</h2>
-          <a href="/about">Cartelera</a>
-          <a href="/about">Candy</a>
-          <a href="/cinePlus">CinePlus</a>
-          <a href="/login">LogIn</a>
+          <Link to="/">Cartelera</Link>
+          <Link to="/">Candy</Link>
+          <Link to="/cinePlus">CinePlus</Link>
+          <Link to="/login">Login</Link>
         </div>
         <div className={style.box__footer}>
             <h2>¿Quienes somos?</h2>
-            <a href="/about">Programadores</a>
-            <a href="https://github.com/sebatora/CineGO"><FontAwesomeIcon icon={faGithub} /> Repositorio</a>
+            <Link to="/about">Programadores</Link>
+            <Link to="https://github.com/sebatora/CineGO" target="_blank"><FontAwesomeIcon icon={faGithub} /> Repositorio</Link>
         </div>
       </div>
 
--- client/src/components/MoviesContainer/MoviesContainer.jsx
+++ client/src/components/MoviesContainer/MoviesContainer.jsx
@@ -1,46 +1,44 @@
-import axios from "axios";
 import React, { useEffect, useState } from "react";
 import style from "./MoviesContainer.module.css";
 import MovieCard from "../MovieCard/MovieCard";
 import Spinner from "../Spinner/Spinner";
 
-import { useSelector } from "react-redux";
+
+import { useDispatch, useSelector } from "react-redux";
+import { getMovies } from "../../redux/actions";
+import Filter from "../Filter/Filter";
 
 function MoviesContainer() {
-  const [movies, setMovies] = useState([]);
+
   const [loading, setLoading] = useState(true);
+  const { allMovies } = useSelector((state) => state);
 
-  const { allMovies } = useSelector(state => state)
+  const dispatch = useDispatch();
+
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get("http://localhost:3001/movies");
-        setMovies(response.data);
-        setLoading(false);
-      } catch (error) {
-        alert(error);
-      }
-    };
+    dispatch(getMovies());
+    setLoading(false)
+  }, [dispatch]);
 
-    fetchData();
-  }, []);
-const Movies = allMovies? allMovies: movies;
   return (
     <div className={style.containerMovies}>
       {loading ? (
         <Spinner />
       ) : (
-        <div className={style.containerCardMovie}>
-          {Movies.map(({ id, title, description, image, release_date }) => (
-            <MovieCard
-              key={id}
-              id={id}
-              title={title}
-              description={description}
-              image={image}
-              release_date={release_date}
-            />
-          ))}
+        <div>
+          <Filter />
+          <div className={style.containerCardMovie}>
+            {allMovies.map(({ id, title, description, image, release_date }) => (
+              <MovieCard
+                key={id}
+                id={id}
+                title={title}
+                description={description}
+                image={image}
+                release_date={release_date}
+              />
+            ))}
+          </div>
         </div>
       )}
     </div>
