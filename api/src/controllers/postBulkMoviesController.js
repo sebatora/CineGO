64f1ae932f356515945535e2cacdc45e7c347ca6
Sebatora <sebastiana.toranzo@gmail.com>
2023-06-15 11:44:30 -0300

    modificacion de la ruta postBulk para que no repita peliculas. SIN FINALIZAR

--- api/src/controllers/postBulkMoviesController.js
+++ api/src/controllers/postBulkMoviesController.js
@@ -3,22 +3,35 @@ const { dataAllMovies } = require("../data/data.js")
 
 const postBulkMovies = async () => {
   const allMovies = await Movie.bulkCreate(dataAllMovies);
-  
+
   for (const movieData of dataAllMovies) {
-    const movieGenres = movieData.genres || [];
-    const movie = allMovies.find(movie => movie.title === movieData.title);
+        const movieGenres = movieData.genres || [];
+        const movie = allMovies.find(movie => movie.title === movieData.title);
+    
+        for (const genreName of movieGenres) {
+              const genre = await Genre.findOne({ where: { name: genreName } });
+      
+              if (genre) {
+                    await movie.addGenre(genre);
+                  }
+                }
+              }
 
-    for (const genreName of movieGenres) {
-      const genre = await Genre.findOne({ where: { name: genreName } });
+        // const moviesCreated = dataAllMovies.forEach(({title, description, image, actors, director, duration, release_date, trailer, clasification}) => {
+        //   Movie.findOrCreate({
+        //     where: {
+        //       title, description, image, actors, director, duration, release_date, trailer, clasification
+        //     },
+        //   });
+        // }); 
+      
+        // if(!moviesCreated) return("No hay peliculas nuevas")
+      
+        // const allMovies = moviesCreated.map(movie => {
+        //   movie.title === dataAllMovies.
+        // })
 
-      if (genre) {
-        await movie.addGenre(genre);
-      }
-    }
-  }
-
-
-  return allMovies;
+        return allMovies;
 };
 
 module.exports = postBulkMovies;
