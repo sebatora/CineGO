5d17fc65c0052830f6e07a78bc321ff41394a0a3
FrancoNat <tatoboga@live.com.ar>
2023-06-15 14:11:32 -0300

    filt

--- api/src/handlers/handlerFilterMovies.js
+++ api/src/handlers/handlerFilterMovies.js
@@ -1,37 +1,39 @@
 const getOrderAscending = require('../controllers/getOrderAscending');
 const getOrderDescending = require('../controllers/getOrderDescending');
 
+
 const handlerFilterMovies = async (req, res) => {
     const { order, filterGenre, filterClasification } = req.body;
 
     try{
-        let filtered = null
+        let filtered = null;
         
         if (order){
         
          if (order === "ascending"){
              filtered = await getOrderAscending();
             // return res.status(200).json(ordered);
-          
+            // order = 1 ascendente, descendente, reciente, antiguo
+        
         } else if (order === "descending"){
              filtered = await getOrderDescending();
             // return res.status(200).json(ordered);
         }
-
+        
         // if(filterGenre){
-        //      console.log("hay generos")
-        // }
-
-        // if(filterClasification){
-        //     console.log("estan clasificados")
-        // }
-
+            //      console.log("hay generos")
+            // }
+            
+            // if(filterClasification){
+            //     console.log("estan clasificados")
+            // } ATP, +13, +16
+            
         return res.status(200).json(filtered);
         
     }
 
     } catch (error) {
-      res.status(500).json({ error: 'Error.' });
+      res.status(500).json({ error: 'Error filtrando.' });
     }
 };
 
