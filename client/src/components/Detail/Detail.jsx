713644c41bc275d6b2e6cb39875b711c53d13f4f
Camila Manita <camilamanita3@gmail.com>
2023-06-15 19:53:57 -0300

    Mejoro estilos y cambio data para poder reproducir videos

--- client/src/components/Detail/Detail.jsx
+++ client/src/components/Detail/Detail.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect} from "react";
 import style from './Detail.module.css';
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { cleanDetail, getMovieById } from "../../redux/actions";
 
@@ -20,17 +20,6 @@ function Detail() {
   return (
     <div className={style.container}>
       <div className={style.row}>
-        <div className={style.infoBox}>
-          {/* <img src="https://img.freepik.com/vector-gratis/mancha-acuarela-abstracta-colorida_1035-18218.jpg?w=2000" alt="fondo" className={style.imagenFondo}></img> */}
-          <h2 className={style.name}>
-            <strong>{title}</strong>
-          </h2>
-          <p className={style.description}> {description} </p>
-          <div className={style.buttonContainer}>
-            <button className={style.button} type="submit">¡Comprar entradas!</button>
-          </div>
-          <iframe width="560" height="315" src={trailer} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
-        </div>
         <div className={style.movieInfo}>
           <figure className={style.figure}>
             <img src={image} alt={title} className={style.movieImage} />
@@ -55,6 +44,19 @@ function Detail() {
               <strong>Duración:  </strong>   {duration} min
             </li>
           </ul>
+        </div>
+        <div className={style.infoBox}>
+          {/* <img src="https://img.freepik.com/vector-gratis/mancha-acuarela-abstracta-colorida_1035-18218.jpg?w=2000" alt="fondo" className={style.imagenFondo}></img> */}
+          <h2 className={style.name}>
+            <strong>{title}</strong>
+          </h2>
+          <p className={style.description}> {description} </p>
+          <div className={style.buttonContainer}>
+            <Link to='/login'>
+              <button className={style.button} type="submit">¡Comprar entradas!</button>
+            </Link>
+          </div>
+          <iframe width="560" height="315" src={trailer} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
         </div>
       </div>
     </div>
