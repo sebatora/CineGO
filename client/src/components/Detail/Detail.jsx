91cb4715ea7d6d31a81d449bdf09ea4742dc847e
Camila Manita <camilamanita3@gmail.com>
2023-06-15 16:57:40 -0300

    conecto el Detail con el back

--- client/src/components/Detail/Detail.jsx
+++ client/src/components/Detail/Detail.jsx
@@ -1,39 +1,58 @@
-import React from "react";
+import React, { useEffect} from "react";
 import style from './Detail.module.css';
+import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { cleanDetail, getMovieById } from "../../redux/actions";
 
 function Detail() {
+
+  const dispatch = useDispatch();
+  const { id } = useParams();
+  const { title, description, image, actors, director, duration, trailer, clasification, genres } = useSelector(state => state.movieById);
+
+  useEffect(() => {
+    dispatch(getMovieById(id));
+    return () => dispatch(cleanDetail())
+
+  }, [id]);
+
+
   return (
     <div className={style.container}>
       <div className={style.row}>
         <div className={style.infoBox}>
           {/* <img src="https://img.freepik.com/vector-gratis/mancha-acuarela-abstracta-colorida_1035-18218.jpg?w=2000" alt="fondo" className={style.imagenFondo}></img> */}
           <h2 className={style.name}>
-            <strong>Spiderman a través del universo</strong>
+            <strong>{title}</strong>
           </h2>
-          <p className={style.description}>
-            Después de reunirse con Gwen Stacy, Spider-Man, el amigable vecino de Brooklyn, es transportado a través del Multiverso, donde se encuentra con un equipo de Spider-Gente encargado de proteger su existencia. Pero cuando los héroes difieren acerca de cómo manejar una nueva amenaza, Miles se enfrenta a los otros Spiders y debe redefinir lo que significa ser un héroe para poder salvar a las personas que más ama.
-          </p>
+          <p className={style.description}> {description} </p>
           <div className={style.buttonContainer}>
             <button className={style.button} type="submit">¡Comprar entradas!</button>
           </div>
-          <iframe width="560" height="315" src="https://www.youtube.com/embed/oBmazlyP220" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          <iframe width="560" height="315" src={trailer} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
         </div>
         <div className={style.movieInfo}>
           <figure className={style.figure}>
-            <img src="https://pics.filmaffinity.com/Spider_Man_Cruzando_el_Multiverso-611174657-large.jpg" alt="image" className={style.movieImage} />
+            <img src={image} alt={title} className={style.movieImage} />
           </figure>
           <ul>
             <li>
-              <strong>Título Original:  </strong>  Spiderman a través del universo
+              <strong>Título Original:  </strong>  {title}
             </li>
             <li>
-              <strong>Género:  </strong>   Animación
+              <strong>Género:  </strong> {genres?.map(genre => genre.name).join(', ')}
             </li>
             <li>
-              <strong>Director:  </strong>   Joaquim Dos Santos, Justin K. Thompson, Kemp Powers
+              <strong>Director:  </strong>   {director}
             </li>
             <li>
-              <strong>Duración:  </strong>   144 minutos
+              <strong>Actores:  </strong>   {actors}
+            <li>
+              <strong>Clasificación:  </strong>  {clasification}
+            </li>
+            </li>
+            <li>
+              <strong>Duración:  </strong>   {duration} min
             </li>
           </ul>
         </div>
