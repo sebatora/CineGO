bb9fb003e7615b7c001b98cf8197a562abb45260
Majomon <mauri.monzon91@gmail.com>
2023-06-14 22:49:53 -0300

    Ordenamiento por nombre - Ascendente y descendente

---
+++ api/src/handlers/handlerOrderAscending.js
@@ -0,0 +1,12 @@
+const getOrderAscending = require("../controllers/getOrderAscending");
+
+const handlerOrderAscending = async (req, res) => {
+  try {
+    const order = await getOrderAscending();
+    res.status(200).json(order);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+module.exports = handlerOrderAscending;
