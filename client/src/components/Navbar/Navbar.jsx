593d2704c4165d361016377e01ed192c60d521ea
MatiasMedina02 <rusomedina02@gmail.com>
2023-06-15 20:35:41 -0400

    Estilos del home y searchbar modificados con tailwind, y dark mode integrado

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -29,32 +29,27 @@ function Navbar() {
       </div>
 
       <div className="w-full flex justify-center space-x-10">
-
         <Link  to="/cineplus">
           <h4 className="hover:underline">CinePlus</h4>
         </Link>
-
-        <Link  to="/about">
-          <h4 className="hover:underline">About</h4>
+      </div>
+      
+      {/* Toogle Theme */}
+      <div className="w-20 lg:w-full flex justify-end mr-8 lg:order-1 order-0">
+        <Link className="mx-4" to="/login">
+          <AccountCircleIcon sx={{ fontSize: 25 }} />
         </Link>
-
-        </div>
-        {/* Toogle Theme */}
-        <div className="w-20 lg:w-full flex justify-end mr-8 lg:order-1 order-0">
-          <Link className="mx-4" to="/login">
-            <AccountCircleIcon sx={{ fontSize: 25 }} />
-          </Link>
-          <button onClick={toggleTheme} className={theme === "light" ? "block" : "hidden"}>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} className="w-6 h-6 stroke-yellow-500">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" />
-            </svg>
-          </button>
-          <button onClick={toggleTheme} className={theme === "dark" ? "block" : "hidden"}>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-6 h-6 fill-blue-800">
-              <path fill-rule="evenodd" d="M9.528 1.718a.75.75 0 01.162.819A8.97 8.97 0 009 6a9 9 0 009 9 8.97 8.97 0 003.463-.69.75.75 0 01.981.98 10.503 10.503 0 01-9.694 6.46c-5.799 0-10.5-4.701-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 01.818.162z" clip-rule="evenodd" />
-            </svg>
-          </button>
-        </div>
+        <button onClick={toggleTheme} className={theme === "light" ? "block" : "hidden"}>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} className="w-6 h-6 stroke-yellow-500">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" />
+          </svg>
+        </button>
+        <button onClick={toggleTheme} className={theme === "dark" ? "block" : "hidden"}>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-6 h-6 fill-blue-800">
+            <path fill-rule="evenodd" d="M9.528 1.718a.75.75 0 01.162.819A8.97 8.97 0 009 6a9 9 0 009 9 8.97 8.97 0 003.463-.69.75.75 0 01.981.98 10.503 10.503 0 01-9.694 6.46c-5.799 0-10.5-4.701-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 01.818.162z" clip-rule="evenodd" />
+          </svg>
+        </button>
+      </div>
     </nav>
   )
 }
