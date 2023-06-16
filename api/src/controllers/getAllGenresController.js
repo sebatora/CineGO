14d361d5a4f94628afa1ec36bd3e5aed29fe44c8
Majomon <mauri.monzon91@gmail.com>
2023-06-16 14:26:12 -0300

    Controler userById v1

--- api/src/controllers/getAllGenresController.js
+++ api/src/controllers/getAllGenresController.js
@@ -1,11 +1,10 @@
-const { Genre } =require("../db")
+const { Genre } = require("../db");
 
 const getAllGenres = async () => {
+  const allGenres = await Genre.findAll();
 
-  const allGenres = await Genre.findAll()
-
-	if (allGenres.length === 0) throw Error("No se encontraron generos")
-	return allGenres;
+  if (allGenres.length === 0) throw Error("No se encontraron generos");
+  return allGenres;
 };
 
 module.exports = getAllGenres;
--- api/src/routes/user.routes.js
+++ api/src/routes/user.routes.js
@@ -1,12 +1,15 @@
 const routerUser = require("express").Router();
-const handlerGetAllUsers = require("../handlers/handlerGetAllUsers")
+const handlerGetAllUsers = require("../handlers/handlerGetAllUsers");
+const handlerGetUserById = require("../handlers/handlerGetUserById");
 const handlerPostUser = require("../handlers/handlerPostUser");
 
 //Ruta para traer todos los usuarios
 routerUser.get("/", handlerGetAllUsers);
 
+//Ruta para traer al usuario por ID
+routerUser.get("/:id", handlerGetUserById);
+
 //Ruta para crear usuarios
 routerUser.post("/", handlerPostUser);
-
 
 module.exports = routerUser;
