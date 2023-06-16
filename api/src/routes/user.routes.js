18506b8302913135041bce0e70b4d4aba5789a1e
FrancoNat <tatoboga@live.com.ar>
2023-06-16 13:19:37 -0300

    Ruta de modificar data de usuarios

--- api/src/routes/user.routes.js
+++ api/src/routes/user.routes.js
@@ -1,6 +1,7 @@
 const routerUser = require("express").Router();
 const handlerGetAllUsers = require("../handlers/handlerGetAllUsers")
 const handlerPostUser = require("../handlers/handlerPostUser");
+const handlerPutUserData = require("../handlers/handlerPutUserData");
 
 //Ruta para traer todos los usuarios
 routerUser.get("/", handlerGetAllUsers);
@@ -8,5 +9,7 @@ routerUser.get("/", handlerGetAllUsers);
 //Ruta para crear usuarios
 routerUser.post("/", handlerPostUser);
 
+//Ruta para actualizar usuarios
+routerUser.put("/", handlerPutUserData);
 
 module.exports = routerUser;
