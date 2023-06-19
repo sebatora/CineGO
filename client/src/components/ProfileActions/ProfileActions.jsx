76349f85394ebbc14d60fc1cf79fcde08820806c
Majomon <mauri.monzon91@gmail.com>
2023-06-19 12:43:30 -0300

    v2

--- client/src/components/ProfileActions/ProfileActions.jsx
+++ client/src/components/ProfileActions/ProfileActions.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import style from "./ProfileActions.module.css";
+import { Link } from "react-router-dom";
 function ProfileActions() {
   return (
     <div className={style.container}>
       <h2>Acciones</h2>
       <section className={style.containerButton}>
-        <div className={style.boxButton}>
+        <Link className={style.boxButton} to={"/changeEmail"}>
           <button>Cambiar email</button>
-        </div>
-        <div className={style.boxButton}>
+        </Link>
+        <Link className={style.boxButton} to={"/record"}>
           <button>Ultimas operaciones/movimientos</button>
-        </div>
+        </Link>
       </section>
     </div>
   );
