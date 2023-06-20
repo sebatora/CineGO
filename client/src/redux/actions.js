0081f6e23793f841c197ae1c92f5d43e3fca5d4a
GeorgeDaz <you@example.com>
2023-06-20 12:04:22 -0600

    action Logout done

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -1,115 +1,126 @@
-import { GET_MOVIES, GET_MOVIE_BY_ID, GET_MOVIES_BY_TITLE, CLEAN_DETAIL, GET_GENRES, POST_MOVIE, DELETE_MOVIE, FILTER_ORDER, POST_USER, LOGIN_USER } from "./action-type";
+import {
+  GET_MOVIES,
+  GET_MOVIE_BY_ID,
+  GET_MOVIES_BY_TITLE,
+  CLEAN_DETAIL,
+  GET_GENRES,
+  POST_MOVIE,
+  DELETE_MOVIE,
+  FILTER_ORDER,
+  POST_USER,
+  LOGIN_USER,
+  LOGOUT_USER,
+} from "./action-type";
 
 import axios from "axios";
 
 // Trae todas las peliculas
-export const getMovies = () => {  
+export const getMovies = () => {
   return async (dispatch) => {
     try {
-      const { data } = await axios.get(`/movies`)
+      const { data } = await axios.get(`/movies`);
       return dispatch({ type: GET_MOVIES, payload: data });
-    }
-    catch (error) {
+    } catch (error) {
       return error.message;
     }
   };
 };
 
 // Trae las peliculas por ID
-export const getMovieById = (id) => {  
+export const getMovieById = (id) => {
   return async (dispatch) => {
     try {
-      const { data } = await axios.get(`/movies/${id}`)
+      const { data } = await axios.get(`/movies/${id}`);
       return dispatch({ type: GET_MOVIE_BY_ID, payload: data });
-    }
-    catch (error) {
+    } catch (error) {
       return error.message;
     }
   };
 };
 
 // Trae las peliculas por nombre
-export const getMoviesByName = (name) => {  
+export const getMoviesByName = (name) => {
   return async (dispatch) => {
     try {
-      const { data } = await axios.get(`/movies?title=${name}`)
+      const { data } = await axios.get(`/movies?title=${name}`);
       return dispatch({ type: GET_MOVIES_BY_TITLE, payload: data });
-    }
-    catch (error) {
+    } catch (error) {
       return error.message;
     }
   };
 };
 
 // Limpia el estado del detail
-export const cleanDetail = () => {  
-  return {type: CLEAN_DETAIL}
+export const cleanDetail = () => {
+  return { type: CLEAN_DETAIL };
 };
 
 // Trae los generos
-export const getGenres = () => {  
+export const getGenres = () => {
   return async (dispatch) => {
     try {
-      const { data } = await axios.get(`/genres`)
+      const { data } = await axios.get(`/genres`);
       return dispatch({ type: GET_GENRES, payload: data });
-    }
-    catch (error) {
+    } catch (error) {
       return error.message;
     }
   };
 };
 
 // Crea una nueva pelicula
-export const postMovie = (newMovie) => {  
+export const postMovie = (newMovie) => {
   return async (dispatch) => {
     try {
-      const { data } = await axios.post(`/movies`, newMovie)
+      const { data } = await axios.post(`/movies`, newMovie);
       return dispatch({ type: POST_MOVIE, payload: data });
-    }
-    catch (error) {
+    } catch (error) {
       return error.message;
     }
   };
 };
 
 // Elimina una pelicula
-export const deleteMovie = (id) => {  
+export const deleteMovie = (id) => {
   return async (dispatch) => {
-      const { data } = await axios.delete(`/movies/${id}`)
-      return dispatch({ type: DELETE_MOVIE, payload: data });
+    const { data } = await axios.delete(`/movies/${id}`);
+    return dispatch({ type: DELETE_MOVIE, payload: data });
   };
 };
 
 // Filtra y ordena
 export const filterOrder = (info) => {
   return async (dispatch) => {
-    const { data } = await axios.post(`/order`, info)
+    const { data } = await axios.post(`/order`, info);
     return dispatch({ type: FILTER_ORDER, payload: data });
   };
 };
 
 // Crea un nuevo usuario
-export const postUser = (newUser) => {  
+export const postUser = (newUser) => {
   return async (dispatch) => {
     try {
-      const { data } = await axios.post(`/users`, newUser)
+      const { data } = await axios.post(`/users`, newUser);
       return dispatch({ type: POST_USER, payload: data });
-    }
-    catch (error) {
+    } catch (error) {
       return error.message;
     }
   };
 };
 
 // Valida el login del usuario
-export const loginUser = (user) => {  
+export const loginUser = (user) => {
   return async (dispatch) => {
     try {
-      const { data } = await axios.post(`/users/login`, user)
+      const { data } = await axios.post(`/users/login`, user);
       return dispatch({ type: LOGIN_USER, payload: data });
-    }
-    catch (error) {
+    } catch (error) {
       return error.message;
     }
   };
+};
+
+// limpia userData
+
+export const logoutUser = () => {
+  return { type: LOGOUT_USER };
 };
