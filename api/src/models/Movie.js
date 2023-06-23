9fdfebc4aeb71a5ab208d55275723109bf0739e4
Sebatora <sebastiana.toranzo@gmail.com>
2023-06-23 15:02:31 -0300

    deploy

--- api/src/models/Movie.js
+++ api/src/models/Movie.js
@@ -11,6 +11,7 @@ module.exports = (sequelize) => {
       title: {
         type: DataTypes.STRING,
         allowNull: false,
+        unique: true,
       },
       description: {
         type: DataTypes.TEXT,
