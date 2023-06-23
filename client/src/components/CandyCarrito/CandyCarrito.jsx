7c018ee62a00ede3e50042aaf3cec54302a0a4b2
Camila Manita <camilamanita3@gmail.com>
2023-06-23 01:09:41 -0300

    Confien, porque si no confian no hay confianza

--- client/src/components/CandyCarrito/CandyCarrito.jsx
+++ client/src/components/CandyCarrito/CandyCarrito.jsx
@@ -1,12 +1,17 @@
 import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 import { addCartCandy, removeAllCartCandy, removeOneCartCandy } from "../../redux/actions";
+import { GoTrash } from "react-icons/go";
 
 function CandyCarrito() {
   const dispatch = useDispatch();
   const cart = useSelector((state) => state.cart);
 
-  let total = cart.reduce((acc, el) => acc + el.price, 0);
+  let subtotal = cart.reduce((acc, el) => acc + el.price, 0);
+
+  let servicio = subtotal * 0.10;
+
+  let total = subtotal + servicio;
 
   const addCart = (name) => {
     dispatch(addCartCandy(name));
@@ -30,47 +35,62 @@ function CandyCarrito() {
   }
 
   return (
-    <div className="w-1/3 mt-28 flex flex-col items-center ">
-      <div className="w-80 mx-auto rounded overflow-hidden shadow-lg bg-white dark:bg-black dark:shadow-gray-700 flex flex-col">
-        <p className=" px-2 py-2 font-bold text-2xl text-gray-700 dark:text-gray-300">
+    <div className="w-1/3 mt-36 flex flex-col items-center">
+      <div className="w-96 mx-auto rounded overflow-hidden shadow-lg bg-gray-50 dark:bg-black dark:shadow-gray-700 flex flex-col">
+        <p className="px-2 py-2 font-bold text-center text-3xl text-gray-700 dark:text-gray-300">
           Candy
         </p>
-        <div className=" px-2 py-1 font-bold text-lg mb-1 text-gray-700 dark:text-white">
-          Productos seleccionados:{" "}
+        <div className="px-2 py-1 font-bold text-lg mb-1 text-gray-700 dark:text-white">
+          Productos seleccionados: {" "}
         </div>
         <div className="px-2 py1">
+          <hr />
           {cart?.map((item, index) => (
-            <div key={index}>
-              <p className="text-sm font-bold  text-gray-700 dark:text-white">
-                {" "}
-                {item.count} {item.name} : {item.price}
-              </p>
-              <button onClick={() => delRemoveCart(item.name, true)}> X </button>
-              <button onClick={() => delRemoveCart(item.name)}> - </button>
-              <button onClick={() => addCart(item.name)}> + </button>
+            <div key={index} className="flex items-center border-b pb-0">
+              <button
+                onClick={() => delRemoveCart(item.name, true)}
+                className="rounded-full h-6 w-6 mr-3 ml-1 mt-1 bg-gray-200 hover:bg-gray-300 flex items-center justify-center text-gray-700 font-semibold transition-colors duration-300"
+              >
+                <GoTrash className="text-xl mx-1" />
+              </button>
+              <div className="mr-4 mt-2 font-bold text-sm">
+                {item.count}{" "} {item.name}
+              </div>
+              <div className="flex items-center my-2 ml-auto">
+                <p className="mr-1 mt-2 text-sm font-bold text-gray-700 dark:text-white">$ {item.price}</p>
+                <button
+                  onClick={() => delRemoveCart(item.name)}
+                  className="rounded-full h-6 w-6 ml-1 mr-2 mt-2 bg-gray-200 hover:bg-gray-300 flex items-center justify-center text-gray-700 font-semibold transition-colors duration-300"
+                >
+                  -
+                </button>
+                <button
+                  onClick={() => addCart(item.name)}
+                  className="rounded-full h-6 w-6 ml-1 mr-2 mt-2 bg-gray-200 hover:bg-gray-300 flex items-center justify-center text-gray-700 font-semibold transition-colors duration-300"
+                >
+                  +
+                </button>
+              </div>
             </div>
           ))}
         </div>
-        <hr />
-        <div className=" px-2 pt-2 font-bold text-sm mb-1 text-gray-700 dark:text-white">
-          Subtotal: $ {" "}
-        </div>
-        <div className=" px-2 font-bold text-sm mb-1 text-gray-700 dark:text-white">
-          Cargo por servicio candy:{" "}
-        </div>
-        <div className=" px-2 font-bold text-lg mb-1 text-gray-700 dark:text-white">
-          <strong>TOTAL: {total} </strong>
-        </div>
 
-                <button
-                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded text-xs"
-                  onClick={handlePay}
-                >
-                  Comprar
-                </button>
-
-          
+        <div className="px-2 pt-2 font-bold text-sm mb-1 text-gray-700 dark:text-white">
+          Subtotal: $ {subtotal}
         </div>
+        <div className="px-2 font-bold text-sm mb-1 text-gray-700 dark:text-white">
+          Cargo por servicio candy: $ {servicio}
+        </div>
+        <div className="px-2 font-bold text-lg mb-1 text-gray-700 dark:text-white">
+          <strong>TOTAL: $ {total} </strong>
+        </div>
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-2 rounded text-xs"
+          onClick={handlePay}
+        >
+          Comprar
+        </button>
+      </div>
     </div>
   );
 }
--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -6,7 +6,6 @@ import cinego_negro from "../../assets/cinego_negro.png"
 import '@fortawesome/fontawesome-free/css/all.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
-import { SiTailwindcss, SiRedux, SiVite, SiFirebase, SiMercadopago, SiReact, SiHtml5, SiCss3, SiJavascript, SiGit, SiGithub, SiTrello, SiFigma, SiNodedotjs, SiPostgresql, SiSequelize } from "react-icons/si";
 
 
 function Footer({ theme }) {
@@ -15,7 +14,7 @@ function Footer({ theme }) {
       <div className="text-center px-40">
         <hr />
       </div>
-      <div className="flex flex-wrap justify-between mx-36 mt-8 mb-8">
+      <div className="flex flex-wrap justify-around mx-48 mt-1 mb-1">
         <div className={style.box__footer}>
           <div className={style.logo}>
           <Link to="/">
@@ -26,40 +25,17 @@ function Footer({ theme }) {
             )}
           </Link>
           </div>
-          <div className={style.terms}>
-            <p>Tecnologías utilizadas</p>
-            <div className={style.iconContainer}>
-                <Link to="https://es.react.dev" target="_blank"><SiReact className={style.icon} /> </Link>
-                <Link to="https://es.react.dev" target="_blank"><SiRedux className={style.icon} /> </Link>
-                <Link to="https://nodejs.org/es" target="_blank"><SiNodedotjs className={style.icon} /> </Link>
-                <Link to="https://developer.mozilla.org/es/docs/Web/HTML" target="_blank"><SiHtml5 className={style.icon} /> </Link>
-                <Link to="https://developer.mozilla.org/es/docs/Web/CSS" target="_blank"><SiCss3 className={style.icon} /> </Link>
-                <Link to="https://developer.mozilla.org/es/docs/Web/JavaScript" target="_blank"><SiJavascript className={style.icon} /> </Link>
-                <Link to="https://git-scm.com" target="_blank"><SiGit className={style.icon}/> </Link>
-                <Link to="https://www.postgresql.org" target="_blank"><SiPostgresql className={style.icon}/> </Link>
-            </div>
-            <div className={style.iconContainer}>
-                <Link to="https://sequelize.org" target="_blank"><SiSequelize className={style.icon}/> </Link>
-                <Link to="https://github.com" target="_blank"><SiGithub className={style.icon}/> </Link>
-                <Link to="https://vitejs.dev" target="_blank"><SiVite className={style.icon}/> </Link>
-                <Link to="https://firebase.google.com/?hl=es" target="_blank"><SiFirebase className={style.icon}/> </Link>
-                <Link to="https://tailwindcss.com" target="_blank"><SiTailwindcss className={style.icon}/> </Link>
-                <Link to="https://trello.com" target="_blank"><SiTrello className={style.icon}/> </Link>
-                <Link to="https://www.figma.com/" target="_blank"><SiFigma className={style.icon}/> </Link>
-                <Link to="https://www.mercadopago.com.ar" target="_blank"><SiMercadopago className={style.icon}/> </Link>
-            </div>
-          </div>
         </div>
 
         <div className={style.box__footer}>
-          <h2 className="mb-24 text-black font-bold text-2xl">Opciones</h2>
+          {/* <h2 className="mb-24 text-black font-bold text-2xl">Opciones</h2> */}
           <Link to="/">Cartelera</Link>
           <Link to="/">Candy</Link>
           <Link to="/cinePlus">CinePlus</Link>
           <Link to="/login">Login</Link>
         </div>
         <div className={style.box__footer}>
-            <h2 className="mb-24 text-black font-bold text-2xl">¿Quienes somos?</h2>
+            {/* <h2 className="mb-24 text-black font-bold text-2xl">¿Quienes somos?</h2> */}
             <Link to="/about">Programadores</Link>
             <Link to="https://github.com/sebatora/CineGO" target="_blank"><FontAwesomeIcon icon={faGithub} /> Repositorio</Link>
         </div>
@@ -67,7 +43,7 @@ function Footer({ theme }) {
 
       <div className="max-w-1200 mx-auto text-center px-40">
         <hr />
-        <p className="mt-20 text-gray-500">Todos los derechos reservados © 2023 <b>| CineGo</b></p>
+        <p className="mt-2 mb-2 text-gray-500">Todos los derechos reservados © 2023 <b>| CineGo</b></p>
       </div>
     </footer>
   )
