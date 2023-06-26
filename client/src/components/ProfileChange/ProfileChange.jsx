cd7ad2129e60723376e625cb871dd993365284f6
Majomon <mauri.monzon91@gmail.com>
2023-06-26 19:14:11 -0300

    Cambios menores

--- client/src/components/ProfileChange/ProfileChange.jsx
+++ client/src/components/ProfileChange/ProfileChange.jsx
@@ -1,14 +1,16 @@
 import React, { useState } from "react";
+import { BiCheckCircle, BiErrorCircle } from "react-icons/bi";
 import { useDispatch } from "react-redux";
-import { logoutUser, putUser } from "../../redux/actions";
-import style from "./ProfileChange.module.css";
 import Swal from "sweetalert2";
+import { logoutUser, putUser } from "../../redux/actions";
+import ProfileBoxHelp from "../ProfileBoxHelp/ProfileBoxHelp";
+import style from "./ProfileChange.module.css";
+import { validateField } from "./validate";
 
 function ProfileChange() {
   const userData = JSON.parse(window.localStorage.getItem("user"));
   const dispatch = useDispatch();
-  /*   const userData = JSON.parse(window.localStorage.getItem("user")); */
-
+  const [error, setError] = useState({});
   const [user, setUser] = useState({
     id: userData.id,
     firstName: "",
@@ -24,6 +26,11 @@ function ProfileChange() {
       ...user,
       [name]: value,
     });
+
+    setError((prevError) => ({
+      ...prevError,
+      [name]: validateField(name, value),
+    }));
   }
 
   const handleSubmit = (e) => {
@@ -70,76 +77,130 @@ function ProfileChange() {
       <div className={style.containerH2}>
         <h2 className={style.h2}>Editar perfil</h2>
       </div>
-      <div>
+      <div className={style.containerProfileAndInfo}>
         <form className={style.containerForm} onSubmit={handleSubmit}>
           <div className={style.containerImage}>
-            <img className={style.image} src={userData.image} />
-            <div className={style.boxButton}>
-              <button
-                type="button"
-                className={`${style.buttonChange} ${style.buttonImg}`}
-              >
-                Cambiar
-              </button>
-              <button
-                type="button"
-                className={`${style.buttonDelete} ${style.buttonImg}`}
-              >
-                Borrar
-              </button>
+            <div className={style.boxImage}>
+              <img className={style.image} src={userData.image} />
+              <div className={style.boxButton}>
+                <button
+                  type="button"
+                  className={`${style.buttonChange} ${style.buttonImg}`}
+                >
+                  Cambiar
+                </button>
+                <button
+                  type="button"
+                  className={`${style.buttonDelete} ${style.buttonImg}`}
+                >
+                  Borrar
+                </button>
+              </div>
             </div>
+            <ProfileBoxHelp />
           </div>
-          <div className={style.containerEditProfile}>
-            <div className={style.box}>
-              <label className={style.labelBox} htmlFor="firstName">
-                Nombre:
-              </label>
-              <input
-                className={style.inputBox}
-                type="text"
-                name="firstName"
-                value={user.firstName}
-                placeholder={userData.firstName}
-                onChange={handleChange}
-              />
+          <div className={style.containerInfo}>
+            <div className={style.containerEditProfile}>
+              <div className={style.box}>
+                <div className={style.boxLabelAndInput}>
+                  <label className={style.labelBox} htmlFor="firstName">
+                    Nombre:
+                  </label>
+                  <input
+                    className={style.inputBox}
+                    type="text"
+                    name="firstName"
+                    value={user.firstName}
+                    placeholder={userData.firstName}
+                    onChange={handleChange}
+                  />
+                  {!error.firstName && (
+                    <BiCheckCircle className={style.iconCheck} />
+                  )}
+                </div>
+                <div className={style.containerError}>
+                  {error.firstName && (
+                    <div className={style.boxError}>
+                      <BiErrorCircle className={style.iconError} />
+                      <span className={style.paragraph}>{error.firstName}</span>
+                    </div>
+                  )}
+                </div>
+              </div>
             </div>
-            <div className={style.box}>
-              <label className={style.labelBox} htmlFor="lastName">
-                Apellido:
-              </label>
-              <input
-                className={style.inputBox}
-                type="text"
-                name="lastName"
-                value={user.lastName}
-                placeholder={userData.lastName}
-                onChange={handleChange}
-              />
+            <div className={style.containerEditProfile}>
+              <div className={style.box}>
+                <div className={style.boxLabelAndInput}>
+                  <label className={style.labelBox} htmlFor="lastName">
+                    Apellido:
+                  </label>
+                  <input
+                    className={style.inputBox}
+                    type="text"
+                    name="lastName"
+                    value={user.lastName}
+                    placeholder={userData.lastName}
+                    onChange={handleChange}
+                  />
+                  {!error.lastName && (
+                    <BiCheckCircle className={style.iconCheck} />
+                  )}
+                </div>
+                <div className={style.containerError}>
+                  {error.lastName && (
+                    <div className={style.boxError}>
+                      <BiErrorCircle className={style.iconError} />
+                      <span className={style.paragraph}>{error.lastName}</span>
+                    </div>
+                  )}
+                </div>
+              </div>
             </div>
-            <div className={style.box}>
-              <label className={style.labelBox} htmlFor="email">
-                Email:
-              </label>
-              <input
-                className={style.inputBox}
-                type="text"
-                name="email"
-                value={user.email}
-                placeholder={userData.email}
-                onChange={handleChange}
-              />
+            <div className={style.containerEditProfile}>
+              <div className={style.box}>
+                <div className={style.boxLabelAndInput}>
+                  <label className={style.labelBox} htmlFor="email">
+                    Email:
+                  </label>
+                  <input
+                    className={style.inputBox}
+                    type="text"
+                    name="email"
+                    value={user.email}
+                    placeholder={userData.email}
+                    onChange={handleChange}
+                  />
+                  {!error.email && (
+                    <BiCheckCircle className={style.iconCheck} />
+                  )}
+                </div>
+                <div className={style.containerError}>
+                  {error.email && error.email !== "" && (
+                    <div className={style.boxError}>
+                      <BiErrorCircle className={style.iconError} />
+                      <span className={style.paragraph}>{error.email}</span>
+                    </div>
+                  )}
+                </div>
+              </div>
             </div>
-            <div className={style.box}>
-              <label className={style.labelBox} htmlFor="cinePlus">
-                Suscripción:
-              </label>
-              <input
-                className={style.inputBox}
-                type="text"
-                name="cinePlus"
-                placeholder={userData.cinePlus}
-                disabled
-              />
+            <div className={style.containerEditProfile}>
+              <div className={style.box}>
+                <div className={style.boxLabelAndInput}>
+                  <label className={style.labelBox} htmlFor="cinePlus">
+                    CinePlus:
+                  </label>
+                  <input
+                    className={style.inputBox}
+                    type="text"
+                    name="cinePlus"
+                    value={user.cinePlus}
+                    placeholder={userData.cinePlus}
+                    onChange={handleChange}
+                    disabled
+                  />
+                </div>
+              </div>
             </div>
           </div>
           <div className={style.containerButtonSave}>
--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -1,38 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ProfileChange from "../../components/ProfileChange/ProfileChange";
 import style from "./Profile.module.css";
 import ProfileSubscription from "../../components/ProfileSubscription/ProfileSubscription";
 import ProfileSecurity from "../../components/ProfileSecurity/ProfileSecurity";
 import ProfileRecord from "../../components/ProfileRecord/ProfileRecord";
+import { Navigate } from "react-router-dom";
 
 function Profile() {
+  const userData = JSON.parse(window.localStorage.getItem("user"));
   const [activeComponent, setActiveComponent] = useState("profileChange");
 
   const handleButtonClick = (componentName) => {
     setActiveComponent(componentName);
   };
 
+  if (!userData) {
+    return <Navigate to="/login" />;
+  }
+
   return (
     <div className={style.container}>
       <div className={style.containerProfile}>
         <div className={style.boxTittle}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="icon icon-tabler icon-tabler-user-circle"
-            width="35"
-            height="35"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="#000000"
-            fill="none"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          >
-            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-            <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
-            <path d="M12 10m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
-            <path d="M6.168 18.849a4 4 0 0 1 3.832 -2.849h4a4 4 0 0 1 3.834 2.855" />
-          </svg>
           <h1 className={style.h1}>Perfil</h1>
         </div>
         <div className={style.containerButton}>
