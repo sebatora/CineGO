29436e5424d320d8c33b1c01d677a7c76c69127e
MatiasMedina02 <rusomedina02@gmail.com>
2023-06-27 16:24:30 -0400

    Suscripcion funcionando correctamente

--- api/src/handlers/subscription/handlerPostSubscription.js
+++ api/src/handlers/subscription/handlerPostSubscription.js
@@ -1,9 +1,9 @@
 const postSubscription = require("../../controllers/subscription/postSubscription")
 
 const handlerPostSubscription = async (req, res) => {
-	const subscription = req.body;
+	const { subscription, userData } = req.body;
 	try {
-		const response = await postSubscription(subscription);
+		const response = await postSubscription({ subscription, userData });
 		return res.status(201).json(response);
 	} catch (error) {
 		return res.status(400).json({ error: error.message });
