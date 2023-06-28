42a47bdbe6742400317b40830b78618baee7d662
Sebastian Ariel <sebastiana.toranzo@gmail.com>
2023-06-28 15:54:52 -0300

    Candy arreglo bug estilos

--- client/src/pages/CandyContainer/CandyContainer.jsx
+++ client/src/pages/CandyContainer/CandyContainer.jsx
@@ -28,13 +28,13 @@ function CandyContainer() {
   const addCart = (name) => {
     if (productCount >= 5) {
       toast.error("Has alcanzado el límite de 5 productos en tu carrito.", {
-        duration: 3000
+        duration: 3000,
       });
       return;
     }
     dispatch(addCartCandy(name));
     toast.success("Producto agregado al carrito", {
-      duration: 2000
+      duration: 2000,
     });
     setProductCount(productCount + 1);
   };
@@ -51,9 +51,9 @@ function CandyContainer() {
       ) : (
         <div className="w-full flex">
           <Toaster />
-          <div className="w-2/3 mt-28 flex flex-col items-center ">
+          <div className="w-2/3 mt-20 flex flex-col items-center ">
             <h2>Combos</h2>
-            <div className="flex flex-wrap justify-center mb-10">
+            <div className="flex flex-wrap justify-center">
               {combos.map(({ id, name, description, price, image }) => (
                 <CandyCard
                   key={id}
@@ -67,8 +67,8 @@ function CandyContainer() {
               ))}
             </div>
 
-            <h2>Pochoclos</h2>
-            <div className="flex flex-wrap justify-center mb-10">
+            <h2 className="mt-4">Pochoclos</h2>
+            <div className="flex flex-wrap justify-center">
               {pochoclos.map(({ id, name, description, price, image }) => (
                 <CandyCard
                   key={id}
@@ -82,8 +82,8 @@ function CandyContainer() {
               ))}
             </div>
 
-            <h2>Bebidas</h2>
-            <div className="flex flex-wrap justify-center mb-10">
+            <h2 className="mt-4">Bebidas</h2>
+            <div className="flex flex-wrap justify-center">
               {bebidas.map(({ id, name, description, price, image }) => (
                 <CandyCard
                   key={id}
@@ -97,8 +97,8 @@ function CandyContainer() {
               ))}
             </div>
 
-            <h2>Snacks</h2>
-            <div className="flex flex-wrap justify-center mb-10">
+            <h2 className="mt-4">Snacks</h2>
+            <div className="flex flex-wrap justify-center">
               {snacks.map(({ id, name, description, price, image }) => (
                 <CandyCard
                   key={id}
@@ -112,8 +112,8 @@ function CandyContainer() {
               ))}
             </div>
 
-            <h2>Cafeteria</h2>
-            <div className="flex flex-wrap justify-center mb-10">
+            <h2 className="mt-4">Cafeteria</h2>
+            <div className="flex flex-wrap justify-center">
               {cafeteria.map(({ id, name, description, price, image }) => (
                 <CandyCard
                   key={id}
@@ -127,8 +127,8 @@ function CandyContainer() {
               ))}
             </div>
 
-            <h2>Golosinas</h2>
-            <div className="flex flex-wrap justify-center mb-10">
+            <h2 className="mt-4">Golosinas</h2>
+            <div className="flex flex-wrap justify-center mb-12">
               {golosinas.map(({ id, name, description, price, image }) => (
                 <CandyCard
                   key={id}
@@ -143,7 +143,11 @@ function CandyContainer() {
             </div>
           </div>
 
-          <CandyCarrito addCart={addCart} productCount={productCount} setProductCount={setProductCount} />
+          <CandyCarrito
+            addCart={addCart}
+            productCount={productCount}
+            setProductCount={setProductCount}
+          />
         </div>
       )}
     </>
