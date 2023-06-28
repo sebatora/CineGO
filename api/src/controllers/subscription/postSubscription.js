a688ec27d8d4ba11b1c4b65e1b9a29d3121882d8
Majomon <mauri.monzon91@gmail.com>
2023-06-28 15:02:24 -0300

    Alerta si se quiere suscribir al mismo plan que posee

--- api/src/controllers/subscription/postSubscription.js
+++ api/src/controllers/subscription/postSubscription.js
@@ -1,33 +1,33 @@
 const { mercadopago } = require("../../utils/mercadoPago");
 
 const postSubscription = async ({ subscription, userData }) => {
-	const response = await mercadopago.preferences.create({
-		items: [
-			{
-				title: subscription.type,
-				currency_id: "ARS",
-				quantity: 1,
-				unit_price: Number(subscription.price)
-			}
-		],
-		payer: {
-			name: userData.firstName,
-			surname: userData.lastName,
-			email: userData.email,
-		},
-		back_urls: {
-			// success: "https://cine-go-ten.vercel.app/payment_success",
-			// failure: "https://cine-go-ten.vercel.app/payment_failure",
-			success: "http://localhost:3000/payment_success",
-			failure: "http://localhost:3000/payment_failure",
-			pending: "",
-		},
-		auto_return: "approved",
-	});
+  const response = await mercadopago.preferences.create({
+    items: [
+      {
+        title: subscription.type,
+        currency_id: "ARS",
+        quantity: 1,
+        unit_price: Number(subscription.price),
+      },
+    ],
+    payer: {
+      name: userData.firstName,
+      surname: userData.lastName,
+      email: userData.email,
+    },
+    back_urls: {
+      // success: "https://cine-go-ten.vercel.app/payment_success",
+      // failure: "https://cine-go-ten.vercel.app/payment_failure",
+      success: "http://localhost:3000/payment_success",
+      failure: "http://localhost:3000/payment_failure",
+      pending: "",
+    },
+    auto_return: "approved",
+  });
 
-	if(!response) throw new Error("Error al crear orden");
+  if (!response) throw new Error("Error al crear orden");
 
-	return response.body;
-}
+  return response.body;
+};
 
 module.exports = postSubscription;
