1935a41a3ef453815dd8a94ede51e60b3dcc5a39
Sebatora <sebastiana.toranzo@gmail.com>
2023-06-30 15:43:41 -0300

    RUTA GET all purchases

--- api/src/routes/purchase.routes.js
+++ api/src/routes/purchase.routes.js
@@ -1,7 +1,10 @@
+const handlerGetAllPurchases = require("../handlers/purchase/handlerGetAllPurchases");
 const handlerPostPurchase = require("../handlers/purchase/handlerPostPurchase");
 
 const routerPurchase = require("express").Router();
 
+routerPurchase.get("/", handlerGetAllPurchases);
+
 routerPurchase.post("/", handlerPostPurchase);
-// routerPurchase.get("/all", );
+
 module.exports = routerPurchase;
