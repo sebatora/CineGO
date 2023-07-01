fbd3c96a314f794b399f7a061180f70c9e4cb514
MatiasMedina02 <rusomedina02@gmail.com>
2023-07-01 00:46:23 -0400

    Estructura del CRUD en movies y candy

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -179,7 +179,9 @@ export default {
         /^(fill-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
     },
   ],
-  plugins: [require("tailwindcss-animated")],
-  plugins: [require("@headlessui/tailwindcss")],
+  plugins: [
+    require("tailwindcss-animated"),
+    require("@headlessui/tailwindcss"),
+  ],
 }
 
