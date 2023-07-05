1a7001156d5c30c7d1ff0482eaeddfe5c5973853
MatiasMedina02 <rusomedina02@gmail.com>
2023-07-05 10:43:37 -0400

    Suscripciones solucionadas

--- api/src/handlers/subscription/handlerPutUserSubscription.js
+++ api/src/handlers/subscription/handlerPutUserSubscription.js
@@ -1,17 +1,15 @@
-const putUserSubscription = require('../../controllers/subscription/putUserSubscriptionController');
+const putUserSubscription = require("../../controllers/subscription/putUserSubscriptionController");
 
 const handlerPutUserSubscription = async (req, res) => {
-    console.log(req.body);
-    try {
-        const { id, cinePlus } = req.body;
-        // Llamamos al controlador para modificar el estado 
-        const userSubscription = await putUserSubscription( id, cinePlus );
-        
-        res.status(200).json(userSubscription);
-    
-    } catch (error) {
-        res.status(400).json({ error : error.message });
-    }
-}
+  try {
+    const { id, cinePlus } = req.body;
+    // Llamamos al controlador para modificar el estado
+    const userSubscription = await putUserSubscription(id, cinePlus);
+
+    res.status(200).json(userSubscription);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
 module.exports = handlerPutUserSubscription;
