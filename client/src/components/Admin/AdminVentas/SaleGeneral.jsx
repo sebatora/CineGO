26aa9d0d8c87b6d54c8af56ff4a60405ab5cbaa9
Camila Manita <camilamanita3@gmail.com>
2023-07-05 10:31:26 -0300

    Incorporo ganancias totales de candy y entradas

--- client/src/components/Admin/AdminVentas/SaleGeneral.jsx
+++ client/src/components/Admin/AdminVentas/SaleGeneral.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { getPurchases } from '../../../redux/actions';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getMetrics, getPurchases } from "../../../redux/actions";
 import {
   Card,
   Table,
@@ -12,12 +12,16 @@ import {
   TableCell,
 } from "@tremor/react";
 
-const valueFormatter = (number) => `$ ${number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")}`;
+const valueFormatter = (number) =>
+  `$ ${number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")}`;
 
 function SaleGeneral() {
-
-  const allPurchases = useSelector((state) => state.allPurchases);
   const dispatch = useDispatch();
+  const allPurchases = useSelector((state) => state.allPurchases);
+  const data = useSelector((state) => state.allMetrics);
+
+  const ticketData = data.filter((item) => item.type === "show");
+  const candyData = data.filter((item) => item.type === "candy");
 
   function calculateTotalPrice(items) {
     let totalPrice = 0;
@@ -29,11 +33,12 @@ function SaleGeneral() {
 
   useEffect(() => {
     dispatch(getPurchases());
+    dispatch(getMetrics());
   }, [dispatch]);
 
   return (
     <>
-       <div className=' overflow-y-auto bg-gradient-to-r from-blue-400 via-pink-400 to-red-400 justify-center item-center'>
+      <div className=" overflow-y-auto bg-gradient-to-r from-blue-400 via-pink-400 to-red-400 justify-center item-center">
         <Card className="bg-blue-200 overflow-y-auto max-w-7xl ml-32 mt-3 ">
           <Title className="text-2xl font-bold text-center mb-2">
             Lista de ventas
@@ -53,43 +58,86 @@ function SaleGeneral() {
               </TableRow>
             </TableHead>
             <TableBody key={1}>
-            {allPurchases.map((item) => (
-              <TableRow
-                key={item.id}
-                className="bg-white hover:bg-gray-100 dark:bg-dark-tremor-brand-muted"
-              >
-                <TableCell className="py-2 px-16 border-b text-center dark:border-gray-500 dark:text-gray-400">
-                 {item.purchase_date.substring(0, 10)}
-                </TableCell>
-                <TableCell className="py-2 px-16 border-b text-center dark:border-gray-500 dark:text-gray-400">
-                  {item.id}
-                </TableCell>
-                <TableCell className="py-2 px-16 border-b text-center dark:border-gray-500 dark:text-gray-400">
-                  {valueFormatter(item.items.length > 0 ? calculateTotalPrice(item.items) : '')}
-                </TableCell>
-              </TableRow>
-            ))}
+              {allPurchases.map((item) => (
+                <TableRow
+                  key={item.id}
+                  className="bg-white hover:bg-gray-100 dark:bg-dark-tremor-brand-muted"
+                >
+                  <TableCell className="py-2 px-16 border-b text-center dark:border-gray-500 dark:text-gray-400">
+                    {item.purchase_date.substring(0, 10)}
+                  </TableCell>
+                  <TableCell className="py-2 px-16 border-b text-center dark:border-gray-500 dark:text-gray-400">
+                    {item.id}
+                  </TableCell>
+                  <TableCell className="py-2 px-16 border-b text-center dark:border-gray-500 dark:text-gray-400">
+                    {valueFormatter(
+                      item.items.length > 0
+                        ? calculateTotalPrice(item.items)
+                        : ""
+                    )}
+                  </TableCell>
+                </TableRow>
+              ))}
             </TableBody>
           </Table>
         </Card>
       </div>
       <div className="flex h-screen bg-gradient-to-r from-blue-400 via-pink-400 to-red-400 overflow-y-auto justify-center item-center">
-        <Card className="bg-blue-200 max-w-xs mt-6 h-40 ">
+        <Card className="bg-blue-200 max-w-xs m-5 ">
           <Title className="text-center">Total recaudado</Title>
           <Table className="h-8 mt-5">
             <TableBody>
               <TableRow>
-                <Title className='text-center'>
-                {valueFormatter(
-                  allPurchases.reduce((total, item) => {
-                    const quantity1 = item.items.length > 0 ? item.items[0].quantity : 0;
-                    const price1 = item.items.length > 0 ? item.items[0].price : 0;
-                    const quantity2 = item.items.length > 1 ? item.items[1].quantity : 0;
-                    const price2 = item.items.length > 1 ? item.items[1].price : 0;
-                    const totalPrice = quantity1 * price1 + quantity2 * price2;
-                    return total + totalPrice;
-                  }, 0)
-                )}
+                <Title className="text-center">
+                  {valueFormatter(
+                    allPurchases.reduce((total, item) => {
+                      const quantity1 =
+                        item.items.length > 0 ? item.items[0].quantity : 0;
+                      const price1 =
+                        item.items.length > 0 ? item.items[0].price : 0;
+                      const quantity2 =
+                        item.items.length > 1 ? item.items[1].quantity : 0;
+                      const price2 =
+                        item.items.length > 1 ? item.items[1].price : 0;
+                      const totalPrice =
+                        quantity1 * price1 + quantity2 * price2;
+                      return total + totalPrice;
+                    }, 0)
+                  )}
+                </Title>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </Card>
+        <Card className="bg-blue-200 max-w-xs m-5">
+          <Title>Total recaudado en entradas</Title>
+          <Table className="h-8 mt-5">
+            <TableBody>
+              <TableRow>
+                <Title className="text-center">
+                  {valueFormatter(
+                    ticketData.reduce((total, item) => {
+                      const totalPrice = item.price * item.quantity;
+                      return total + totalPrice;
+                    }, 0)
+                  )}
+                </Title>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </Card>
+        <Card className="bg-blue-200 max-w-xs m-5">
+          <Title>Total recaudado en candy</Title>
+          <Table className="h-8 mt-5">
+            <TableBody>
+              <TableRow>
+                <Title className="text-center">
+                  {valueFormatter(
+                    candyData.reduce((total, item) => {
+                      const totalPrice = item.price * item.quantity;
+                      return total + totalPrice;
+                    }, 0)
+                  )}
                 </Title>
               </TableRow>
             </TableBody>
@@ -97,7 +145,7 @@ function SaleGeneral() {
         </Card>
       </div>
     </>
-  )
+  );
 }
 
-export default SaleGeneral
+export default SaleGeneral;
