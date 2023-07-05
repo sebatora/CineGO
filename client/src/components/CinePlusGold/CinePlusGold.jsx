15f9c8bc66462689cd517643c0f0329a03ff532d
Majomon <mauri.monzon91@gmail.com>
2023-07-05 16:29:24 -0300

    Esta si es la buena del cinePlus

--- client/src/components/CinePlusGold/CinePlusGold.jsx
+++ client/src/components/CinePlusGold/CinePlusGold.jsx
@@ -58,9 +58,15 @@ function CinePlusGold() {
       <div className="w-96 mt-2 rounded-lg relative">
         <div className="">
           <div className="h-20 grid items-center bg-yellow-300 border-2 border-yellow-300 rounded-t-xl">
-            <h2 className="font-bold mx-auto">Cine Plus Gold</h2>
+            <h2 className="font-bold mx-auto text-white dark:text-black">
+              Cine Plus Black
+            </h2>
             {pathname !== "/profile" ? (
-              <span className="font-bold mx-auto text-black dark:text-white">
+              <span className="font-bold mx-auto text-white dark:text-black">
+                ${subGold.price} por mes
+              </span>
+            ) : userData.cinePlus === "Estandar" ? (
+              <span className="font-bold mx-auto text-white dark:text-black">
                 ${subGold.price} por mes
               </span>
             ) : (
